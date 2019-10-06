a6f38e8c4e796eedd3c24bde4901668adfc1026f
Natan Farkash <natqe3@gmail.com>
2019-10-06 14:41:56 +0300

    export operate

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -1,10 +1,12 @@
 import { Delete } from './delete'
 import { Upsert } from './upsert'
 import { Load } from './load'
+import { Operate } from './operate'
 import { DataService, AbstractDataService } from './service'
 
 const rxDataService = {
   DataService,
+  Operate,
   Delete,
   Upsert,
   Load,
@@ -15,6 +17,7 @@ export {
   rxDataService,
   DataService,
   Delete,
+  Operate,
   Upsert,
   Load,
   AbstractDataService
